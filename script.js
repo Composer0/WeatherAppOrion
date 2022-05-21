ea9b5f87cd841ce96588868b581ea1ad5d026f74
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-05-21 11:03:31 -0400

    Update

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
-// modules
-const express = require('express');
-const https = require('https');
-const bodyParser = require('body-parser')
+let appKey = "key=9c6045f2530f4a24a81130713221505"
+let query = req.body.cityName
+let searchTerm = search.value
+let url = "https://api.weatherapi.com/v1/current.json?" + appKey + "&q=" + searchTerm
+const defaultURL = 'https://api.weatherapi.com/v1/current.json?key=9c6045f2530f4a24a81130713221505&q=Aiken'
 
-const app = express();
+// values being gathered for app.
+let city = weatherData.location.name
+let currentDay = weatherData.current.is_day
+let conditionText = weatherData.current.condition.text
+let icon = weatherData.current.condition.icon
+let tempC = weatherData.current.temp_c
+let tempF = weatherData.current.temp_f
+let windMPH = weatherData.current.wind_mph
+let windKPH = weatherData.current.wind_kph
 
-// necessary code to pass through the body of the post request.
-app.use(bodyParser.urlencoded({ extended: true}));
 
-const { STATUS_CODES } = require('https');
-// node module that allows the use and access of the get request from a https protocol for a website based api.
 
-app.use(express.static())
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
-    
-    
-})
+// // This will be need later to swap between celcius and fahrenheit data.
 
-app.post("/", function(req,res){
-    // api information. Has to be in the app.post section when being created.
-    const starterAPI = "https://api.weatherapi.com/v1/current.json?key=9c6045f2530f4a24a81130713221505&q=Aiken"
+let celsius = document.querySelector("#celcius");
+celsius.addEventListener("click", convertToCelcius);
 
-    const appKey = "key=9c6045f2530f4a24a81130713221505"
-    const query = req.body.cityName
-    const url = "https://api.weatherapi.com/v1/current.json?" + appKey + "&q=" + query
-    https.get(url, function(response) {
-        console.log(response.statusCode);
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", convertToFahrenheit);
 
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data)
-            // JSON.parse(data) allows the data to be converted to be findable as JS objects. With this the following inputs below are possible because we are able to pull the data from the API and insert them into our DOM or console.log.
-            const city = weatherData.location.name
-            const region = weatherData.location.region
-            const country = weatherData.location.country
-            const tempC = weatherData.current.temp_c
-            const tempF = weatherData.current.temp_f
-            const currentDay = weatherData.current.is_day
-            const windMPH = weatherData.current.wind_mph
-            const windKPH = weatherData.current.wind_kph
-            const conditionText = weatherData.current.condition.text
-            const icon = weatherData.current.condition.icon
-            console.log(city, region, country, tempC, tempF, currentDay, windMPH, windKPH, conditionText)
-            res.write("<p>The current weather in " + query + " is " + conditionText + ".<p>");
-            // fun note... having an h2 prior to an h1 in this document will create a json packgage.
-            res.write("<img src='" + icon + "'>")
-            // when creating these inputs to pass information inside of your html files you need to write aspects of it the same way that you would if you writing a simple html document. The only difference is, is the quotations and the addition of your variables.
-            res.write("<h1>The temperature is " + tempF + " degrees farenheit in " + query +".</h1>")
-            res.send(index.html);
-            // only one app res.send method can be used in any given document. choose wisely. You can res.write as many beforehand. res.send is the last action that can be done however. res.send can not be a written item when there is already a res.write occuring.
-        })
-    })
-})
+function convertToCelsius (event){
+    event.preventDefault();
+     if (currentTemp == null){
+      alert("Please search for a city");
+    }
+    fahrenheit.classList.remove("active");
+    celsius.classList.add("active");
+    let displayCelsius= document.querySelector(".currentTemp");
+    tempC = Math.round((currentTemp - 32) * 5/9);
+    displayCelsius.innerHTML= tempC;
+  }
+  
+  function convertToFahrenheit (event) {
+    event.preventDefault();
+    if (currentTemp == null || tempC == null){
+      searchCurrentCity();
+    }
+    celsius.classList.remove("active");
+    fahrenheit.classList.add("active");
+    let displayFahrenheit = document.querySelector(".currentTemp");
+    let tempF = Math.round((celsiusTemp * 9/5) + 32);
+    displayFahrenheit.innerHTML = tempF;
+  }
+
+
+
+
+
+
+
+console.log(city, region, country, tempC, tempF, currentDay, windMPH, windKPH, conditionText)
